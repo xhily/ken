d8c8c37bb7ea0a3b4c312eb868b12b0eba9d4660
wukaipeng <wkpcoder@163.com>
2025-06-24 00:23:13 +0800

    📝 feat: update HomepageHeader component to enhance quote display and remove unused imports for cleaner code

--- src/components/HomepageHeader/index.tsx
+++ src/components/HomepageHeader/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import clsx from 'clsx'
-import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import Translate from '@docusaurus/Translate'
+// import Link from '@docusaurus/Link'
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+// import Translate from '@docusaurus/Translate'
 
 import styles from './styles.module.scss'
 
 export default function HomepageHeader(): React.ReactElement {
-  const { siteConfig } = useDocusaurusContext()
+  // const { siteConfig } = useDocusaurusContext()
 
   return (
     <header className={clsx( styles.heroBanner)}>
-      <div className='container'>
+      {/* <div className='container'>
         <p className='hero__title'>
           "<Translate id="homepage.quote.line1">你写的每一行代码</Translate>
         </p>
         <p className='hero__title'>
           <Translate id="homepage.quote.line2">都是你的名片</Translate>"
         </p>
+      </div> */}
+            <div className='container'>
+        <p className='hero__title'>“你写的每一行代码</p>
+        <p className='hero__title'>都是你的名片”</p>
+        <p className='hero__title'>"Every line of code you write</p>
+        <p className='hero__title'>is your business card"</p>
       </div>
     </header>
   )
